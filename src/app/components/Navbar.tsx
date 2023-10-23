36313b6f027877edebb648b17b8f02cd3537a265
Shaheedha <shaheedhamolshahi@gmail.com>
2023-10-23 11:32:28 +0530

    navbar width fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,7 +8,7 @@
 export default function Navbar(props: any) {
     const { handleClick } = props
     return (
-        <div style={{ height: '2rem', padding: '10px 5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'yellowgreen', position: 'fixed', top: '0', left: '0', right: '0', width: '98vw', zIndex: 10 }}>
+        <div style={{ height: '2rem', padding: '10px 5px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'yellowgreen', position: 'fixed', top: '0', left: '0', width: 'calc(100% - 10px)', zIndex: 10 }}>
             <h3>Unplex.Me</h3>
             <button style={{}} onClick={handleClick}>Save</button>
         </div>
